5220439f83e1efa8fab598c83f64e336add34f84
HimanProgrammer <hb.19editor@gmail.com>
2025-08-02 18:41:25 +0530

    first commit

--- pages/service-single/sidebar.js
+++ pages/service-single/sidebar.js
@@ -1,41 +1,54 @@
-import React from 'react';
-import Link from 'next/link'
-import Services from '../../api/Services';
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import ServicesData from '../../data/services.json';
 
+const ServiceSidebar = () => {
+  const [lang, setLang] = useState('en');
+  const [services, setServices] = useState([]);
 
-const ServiceSidebar = (props) => {
+  useEffect(() => {
+    const selectedLang = localStorage.getItem('selectedLanguage') || 'en';
+    setLang(selectedLang);
+    const langServices = ServicesData[selectedLang] || ServicesData['en'];
+    setServices(langServices);
+  }, []);
 
-    const ClickHandler = () => {
-        window.scrollTo(10, 0);
-    }
+  const ClickHandler = () => {
+    window.scrollTo(10, 0);
+  };
 
-    return (
-        <div className="service-sidebar">
-            <div className="service-catagory">
-                <ul>
-                    {Services.slice(0, 5).map((serves, index) => (
-                        <li key={index}>
-                            <Link href={'/service-single/[slug]'} as={`/service-single/${serves.slug}`}  onClick={ClickHandler}>{serves.title} <i
-                                className="flaticon-right-arrow-1"></i></Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <div className="service-info">
-                <div className="icon">
-                    <i className="flaticon-phone-call"></i>
-                </div>
-                <h2>Looking for
-                    logistics service
-                    Provider?</h2>
-                <span>Call anytime</span>
-                <div className="num">
-                    <span>+(2) 871 382 023</span>
-                </div>
-            </div>
+  return (
+    <div className="service-sidebar">
+      <div className="service-catagory">
+        <ul>
+          {services.slice(0, 5).map((service, index) => (
+            <li key={index}>
+              <Link href={`/service-single/${service.slug}`} onClick={ClickHandler}>
+                {service.title} <i className="flaticon-right-arrow-1"></i>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* <div className="service-info">
+        <div className="icon">
+          <i className="flaticon-phone-call"></i>
         </div>
-    )
-}
+        <h2>
+          Looking for
+          <br />
+          logistics service
+          <br />
+          Provider?
+        </h2>
+        <span>Call anytime</span>
+        <div className="num">
+          <span>+(2) 871 382 023</span>
+        </div>
+      </div> */}
+    </div>
+  );
+};
 
 export default ServiceSidebar;
-
