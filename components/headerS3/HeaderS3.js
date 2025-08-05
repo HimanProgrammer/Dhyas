1a13518a92e5beea7b8daa71ee787663a0008e9c
HimanProgrammer <hb.19editor@gmail.com>
2025-08-05 13:46:46 +0530

    first commit

--- components/headerS3/HeaderS3.js
+++ components/headerS3/HeaderS3.js
@@ -44,18 +44,20 @@ const HeaderS3 = ({ carts, hclass }) => {
         <nav className="navigation navbar mt-60 navbar-expand-lg navbar-light">
           <div className="container-fluid">
             <div className="row align-items-center">
-              <div className="col-lg-3 col-md-3 col-3 d-lg-none dl-block">
-                <MobileMenu />
-              </div>
+              
 
               <div className="col-lg-2 col-md-6 col-6">
                 <div className="navbar-header">
-                  <Link href="/home" onClick={handleClick} className="navbar-brand">
+                  <Link href="/" onClick={handleClick} className="navbar-brand">
                     <Image src={Logo} alt="logo" />
                   </Link>
                 </div>
               </div>
 
+              <div className="col-lg-3 col-md-3 col-3 d-lg-none dl-block">
+                <MobileMenu />
+              </div>
+              
               <div className="col-lg-6 col-md-1 col-1">
                 <div className="collapse navbar-collapse navigation-holder" id="navbar">
                   <ul className="nav navbar-nav mb-2 mb-lg-0">
