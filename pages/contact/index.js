60da6df3ec78ae0df2f381174219280b4704412b
HimanProgrammer <hb.19edior@gmail.com>
2025-08-06 06:16:13 +0530

    first commit

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,7 +1,14 @@
-import pageTitles from '../../data/pageTitles.json';
 import { useEffect, useState } from 'react';
+import React, { Fragment } from 'react';
+import Navbar from '../../components/NavbarS3/NavbarS3';
+import ContactPage from '../../components/Contactpage/Contactpage';
+import Footer from '../../components/footer/Footer';
+import Scrollbar from '../../components/scrollbar/scrollbar'; 
+import Logo from '/public/images/logo-2.svg';
+import PageTitle from '../../components/pagetitle/PageTitle';
+import pageTitles from '../../data/pageTitles.json';
 
-const ContactPage = () => {
+const Contact = () => {
     const [lang, setLang] = useState('en');
     const [titleText, setTitleText] = useState(pageTitles.en);
 
@@ -14,7 +21,7 @@ const ContactPage = () => {
     return (
         <Fragment>
             <Navbar hclass={'wpo-site-header'} Logo={Logo} />
-            <pageTitles pageTitle={titleText.contactPageTitle} pagesub={titleText.contactPageSub} />
+            <PageTitle pageTitle={titleText.contactPageTitle} pagesub={titleText.contactPageSub} />
             <ContactPage />
             <Footer />
             <Scrollbar />
@@ -22,4 +29,4 @@ const ContactPage = () => {
     );
 };
 
-export default ContactPage;
+export default Contact;
